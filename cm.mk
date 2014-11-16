bc9a725fa7cd606c7003b9d5c2fe6e3bcee71fa8
Michael Bestas <mikeioannina@gmail.com>
2014-11-17 01:32:13 +0200

    find7: Remove deprecated gsm.mk include
    Change-Id: I4688eb833cc24ba5bbf2ad448135980d81cd394c

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/oppo/find7/full_find7.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
