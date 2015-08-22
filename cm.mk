2230506ea4abb84535228d92c3c69c759b4fbc43
Michael Bestas <mikeioannina@gmail.com>
2015-08-22 18:16:23 +0300

    find7: Cleanup makefiles
    Change-Id: I8945be9b566f5b39aa714c44ba9e9c15db7b2077

--- cm.mk
+++ cm.mk
@@ -1,13 +1,31 @@
-$(call inherit-product, device/oppo/find7/full_find7.mk)
+# Copyright (C) 2014-2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/oppo/find7/find7.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_NAME := cm_find7
 PRODUCT_DEVICE := find7
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_MODEL := Find7
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
