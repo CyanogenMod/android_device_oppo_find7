7db5af4df7c5a16687bfccc466095236617a3935
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 02:36:33 -0700

    find7: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    RM-290
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8
    (cherry picked from commit fc1ae9425481ded366ca634bd2320feb16e42af6)

--- cm.mk
+++ cm.mk
@@ -18,9 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from find7 device
 $(call inherit-product, device/oppo/find7/find7.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
